5e6f853276ffe67fe1c4134efb9a38908020174e
brduarte <bryan.duarte@etu.uca.fr>
2021-02-05 15:51:56 +0100

    oui

---
+++ ProjetTuT/model/DataStructures/ImageNetPrediction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace TEST2.DataStructures
+{
+    public class ImageNetPrediction
+    {
+        [ColumnName("grid")]
+        public float[] PredictedLabels;
+    }
+}
